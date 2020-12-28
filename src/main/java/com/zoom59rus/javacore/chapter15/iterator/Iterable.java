4acef4fd7c6b2197446036c0de7b8d05ddc1d852
zoom59rus <anton_1@bk.ru>
2020-12-28 20:32:11 +0500

    Шеукфещк Complete

---
+++ src/main/java/com/zoom59rus/javacore/chapter15/iterator/Iterable.java
@@ -0,0 +1,5 @@
+package com.zoom59rus.javacore.chapter15.iterator;
+
+public interface Iterable<T> {
+    Iterator<T> iterator();
+}
---
+++ src/main/java/com/zoom59rus/javacore/chapter15/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.zoom59rus.javacore.chapter15.iterator;
+
+public interface Iterator<E> {
+    boolean hasNext();
+    E next();
+}
