9ae6510b7122c35bb5b05fe7bd270374df7974e7
zoom59rus <anton_1@bk.ru>
2021-01-01 13:42:38 +0500

    Template Complete

---
+++ src/main/java/com/zoom59rus/javacore/chapter15/template/Application.java
@@ -0,0 +1,14 @@
+package com.zoom59rus.javacore.chapter15.template;
+
+public class Application {
+    public static void main(String[] args) {
+        SocialNetwork vk = new Vk();
+        SocialNetwork linkedIn = new LinkedIn();
+        SocialNetwork odnoklassniki = new Odnoklassniki();
+
+        vk.login();
+        linkedIn.login();
+        odnoklassniki.login();
+
+    }
+}
